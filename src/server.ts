d41d4fee0d050ddc8b3a430e58bc672e536b8d62
Walison Miranda <46504580+WalisonMiranda@users.noreply.github.com>
2024-06-28 19:41:17 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,9 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
 
 app.use("/auth", userRouter);
 app.use("/transaction", transactionRouter);
